89fe7233e4e4a02b494a70e02c823ce6b1af80c1
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2015-01-23 14:05:02 -0800

    Fixed AugmentedView drawing on zoom. Changed colors

--- src/ip/c0d/ippg/AugmentedView.java
+++ src/ip/c0d/ippg/AugmentedView.java
@@ -12,6 +12,8 @@ public class AugmentedView extends BitmapView {
     private double thresholdTime;
     private double dilateTime;
     private double findBlobsTime;
+    private static final Matrix matrix = new Matrix() {{ postScale(-1, 1); postTranslate(640, 0); }};
+    private static final float[] matrix_elements = new float[9];
 
     private static final Paint red = new Paint() {{
         setAntiAlias(true);
@@ -24,10 +26,8 @@ public class AugmentedView extends BitmapView {
     }};
     private static final Paint green = new Paint(red) {{ setColor(Color.GREEN); }};
     private static final Paint blue = new Paint(red) {{ setColor(Color.BLUE); }};
-    private static final Paint yellow = new Paint(red) {{ setColor(Color.YELLOW); }};
-//  private static final Paint magenta = new Paint(red) {{ setColor(Color.MAGENTA); }};
-    private static final Matrix matrix = new Matrix() {{ postScale(-1, 1); postTranslate(640, 0); }};
-    private static final Paint text = new Paint(yellow) {{
+    private static final Paint coral1 = new Paint(red) {{ setColor(Colors.Coral1); }};
+    private static final Paint text = new Paint(green) {{
         setStyle(Paint.Style.STROKE);
         setStrokeJoin(Paint.Join.ROUND);
         setStrokeWidth(1f);
@@ -57,20 +57,24 @@ public class AugmentedView extends BitmapView {
     }
 
     protected void onDraw(Canvas c) {
+        int saveCount = c.getSaveCount();
         c.save(Canvas.MATRIX_SAVE_FLAG);
         super.onDraw(c);
+        getImageMatrix().getValues(matrix_elements);
+        matrix_elements[Matrix.MTRANS_X] += getMeasuredWidth() * matrix_elements[Matrix.MSCALE_X];
+        matrix_elements[Matrix.MSCALE_X] = -matrix_elements[Matrix.MSCALE_X];
+        matrix.setValues(matrix_elements);
         c.concat(matrix);
+        getImageMatrix().getValues(matrix_elements);
         for (int i = 0; i < nb; i++) {
             ImageProcessing.Blob b = blobs[i];
 //          trace("blobs[" + i + "] center " + b.centerX + "," + b.centerY);
 //          trace("blobs[" + i + "] numberOfSegments " + b.numberOfSegments + " segmentsStars " + b.segmentsStart);
-            if (i < 100) {
-                c.drawLines(fp, b.segmentsStart * 2, b.numberOfSegments * 2, red);
-            }
-            c.drawRect(b.left, b.top, b.right, b.bottom, green);
-            c.drawRect(b.centerX, b.centerY, b.centerX + 1, b.centerY + 1, blue);
+            c.drawLines(fp, b.segmentsStart * 2, b.numberOfSegments * 2, coral1);
+            c.drawRect(b.left, b.top, b.right, b.bottom, blue);
+            c.drawRect(b.centerX, b.centerY, b.centerX + 1, b.centerY + 1, red);
         }
-        c.restore();
+        c.restoreToCount(saveCount);
         c.drawText(String.format("threshold  %.6f", thresholdTime), 10, 20, text);
         c.drawText(String.format("dilate     %.6f", dilateTime), 10, 40, text);
         c.drawText(String.format("find blobs %.6f", findBlobsTime), 10, 60, text);
